2516104b78329e71aadc44e4c0417436579ee961
Warhell Nasim <warhell1@hotmail.com>
2018-10-10 14:15:05 +0200

    Return Option<RealSense> instead

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 use std::sync::atomic::AtomicPtr;
 
 use std::os::raw::c_int;
+use std::ffi::CStr;
+
 use std::ptr;
 
 use log::Level::Debug;
@@ -22,7 +24,7 @@ impl RealSense {
     pub fn new(
         FPS: c_int,
         CAMERA_WIDTH: c_int,
-        CAMERA_HEIGHT: c_int) -> RealSense {
+        CAMERA_HEIGHT: c_int) -> Option<RealSense> {
         unsafe {
             // calculate API version e.g. 21400
             let api_version: std::os::raw::c_int = (
@@ -30,7 +32,8 @@ impl RealSense {
                 + (RS2_API_MINOR_VERSION * 100)
                 + RS2_API_PATCH_VERSION) as i32;
 
-            let error = ptr::null_mut();
+            let error: *mut *mut rs2_error = ptr::null_mut();
+            //let error = ptr::null_mut();
 
             // Create a context object. This object owns the handles to all connected realsense devices.
             // The returned object should be released with rs2_delete_context(...)
@@ -48,12 +51,12 @@ impl RealSense {
 
             // int dev_count = rs2_get_device_count(device_list, &e);
             info!("Getting device count");
-            let devices_count = rs2_get_device_count(device_list, error);
+            let cameras_count = rs2_get_device_count(device_list, error);
             check_error(error);
 
-            info!("Found {:?} device(s)", devices_count);
+            info!("Found {:?} camera(s)", cameras_count);
 
-            if devices_count == 0 {
+            if cameras_count == 0 {
                 info!("Exiting...");
                 std::process::exit(1);
             }
@@ -61,49 +64,50 @@ impl RealSense {
             // Get the first connected device
             // The returned object should be released with rs2_delete_device(...)
             // rs2_device* dev = rs2_create_device(device_list, 0, &e);
-            info!("Creating RS2 device");
-            let device = rs2_create_device(device_list, 0, error);
-            check_error(error);
+            if let Some(device) = get_realsense_camera(device_list, cameras_count) {                    
+                print_device_info(&*device);
 
-            print_device_info(&*device);
+                // Create a pipeline to configure, start and stop camera streaming
+                // The returned object should be released with rs2_delete_pipeline(...)
+                //rs2_pipeline* pipeline =  rs2_create_pipeline(ctx, &e);
+                info!("Creating pipeline");
+                let pipe = rs2_create_pipeline(ctx, error);
+                check_error(error);
 
-            // Create a pipeline to configure, start and stop camera streaming
-            // The returned object should be released with rs2_delete_pipeline(...)
-            //rs2_pipeline* pipeline =  rs2_create_pipeline(ctx, &e);
-            info!("Creating pipeline");
-            let pipe = rs2_create_pipeline(ctx, error);
-            check_error(error);
+                // Create a config instance, used to specify hardware configuration
+                // The retunred object should be released with rs2_delete_config(...)
+                // rs2_config* config = rs2_create_config(&e);
+                info!("Creating config");
+                let config = rs2_create_config(error);
+                check_error(error);
 
-            // Create a config instance, used to specify hardware configuration
-            // The retunred object should be released with rs2_delete_config(...)
-            // rs2_config* config = rs2_create_config(&e);
-            info!("Creating config");
-            let config = rs2_create_config(error);
-            check_error(error);
+                // Request a specific configuration
+                info!("Enabling stream");
+                rs2_config_enable_stream(
+                    config,
+                    STREAM as u32,
+                    STREAM_INDEX,
+                    CAMERA_WIDTH,
+                    CAMERA_HEIGHT,
+                    FORMAT as u32,
+                    FPS,
+                    error,
+                );
+                check_error(error);
 
-            // Request a specific configuration
-            info!("Enabling stream");
-            rs2_config_enable_stream(
-                config,
-                STREAM as u32,
-                STREAM_INDEX,
-                CAMERA_WIDTH,
-                CAMERA_HEIGHT,
-                FORMAT as u32,
-                FPS,
-                error,
-            );
-            check_error(error);
+                info!("Starting pipeline with config");
+                let _rs2_pipeline_profile = rs2_pipeline_start_with_config(pipe, config, error);
+                check_error(error);
 
-            info!("Starting pipeline with config");
-            let _rs2_pipeline_profile = rs2_pipeline_start_with_config(pipe, config, error);
-            check_error(error);
+                return Some(RealSense { pipeline: pipe});
+            }
 
-            RealSense { pipeline: pipe}
+            error!("No suitable camera found");
+            None
         }
     }
 
-    pub fn run(&mut self) -> std::sync::atomic::AtomicPtr<u8> {
+    pub fn run(&self) -> std::sync::atomic::AtomicPtr<u8> {
         let error = ptr::null_mut();
         let mut rgb_frame_data = ptr::null_mut();
 
@@ -149,9 +153,35 @@ impl RealSense {
     }
 }
 
-fn check_error(e: *mut *mut rs2_error) {
-    debug!("Error? {:?}\n", !e.is_null());
+fn get_realsense_camera(device_list: *mut rs2_device_list, cameras_count: i32) -> Option<*mut rs2_device> {
+    let error = ptr::null_mut();
+    (0..cameras_count)
+        .map(|i| unsafe {
+            let device = rs2_create_device(device_list, i, error);
+            check_error(error);
+            device
+        })
+        .fold(None, |acc, d| {
+            let name = fetch_camera_name(unsafe {&*d});
+            info!("Found device with name: {}", name);
+            if name.contains("Intel RealSense") {
+                info!("Selecting device: {}", name);
+                Some(d)
+            } else {
+                acc
+            }
+        })
+}
 
+fn fetch_camera_name(device: &rs2_device) -> &str {
+    let error = ptr::null_mut();
+    let c_str: &CStr = unsafe { CStr::from_ptr(rs2_get_device_info(device, rs2_camera_info_RS2_CAMERA_INFO_NAME, error)) };
+    check_error(error);
+
+    c_str.to_str().expect("Could not fetch camera info name")
+}
+
+fn check_error(e: *mut *mut rs2_error) {
     if !e.is_null() {
         unsafe {
             error!(
@@ -166,29 +196,32 @@ fn check_error(e: *mut *mut rs2_error) {
 }
 
 fn print_device_info(device: &rs2_device) { 
-    unsafe {
-        let error = ptr::null_mut();
+    info!("Using device 0: {}", fetch_camera_name(device));
 
-        info!(
-            "Using device 0: {:?}",
-            rs2_get_device_info(device, rs2_camera_info_RS2_CAMERA_INFO_NAME, error)
-        );
-        check_error(error);
+    let error = ptr::null_mut();
+    {
+        let c_str: &CStr = unsafe { CStr::from_ptr(
+            rs2_get_device_info(
+                device,
+                rs2_camera_info_RS2_CAMERA_INFO_SERIAL_NUMBER,
+                error))
+        };
+        let str_slice: &str = c_str.to_str().expect("Could not fetch camera serial number");
+        info!("Serial number: {}", str_slice);
+    }
+    check_error(error);
 
-        info!(
-            "Serial number: {:?}",
-            rs2_get_device_info(device, rs2_camera_info_RS2_CAMERA_INFO_SERIAL_NUMBER, error)
-        );
-        check_error(error);
-
-        info!(
-            "Firmware version: {:?}",
+    {
+        let c_str: &CStr = unsafe { CStr::from_ptr(
             rs2_get_device_info(
                 device,
                 rs2_camera_info_RS2_CAMERA_INFO_FIRMWARE_VERSION,
                 error
-            )
-        );
-        check_error(error);
+                ))
+        };
+
+        let str_slice: &str = c_str.to_str().expect("Could not fetch camera firmware version");
+        info!("Firmware version: {}", str_slice);
     }
+    check_error(error);
 }
